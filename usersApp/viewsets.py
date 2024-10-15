6467bb173eae380748cda57ee5df03ab40010680
Alabi Khalid <alabikhalid.12@gmail.com>
2024-10-15 22:07:18 +0100

    set up user registration

--- usersApp/viewsets.py
+++ usersApp/viewsets.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from .serializers import UserSerializers
 
- 
 class UserViewSets(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializers
